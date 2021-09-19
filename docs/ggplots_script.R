91f884b8f254bc34fe87829fbf4dc3b0762b3dfb
EvaMaeRey <evangeline.mae@gmail.com>
2021-09-18 20:59:37 -0400

    add a error to the example code ggplots_script

--- docs/ggplots_script.R
+++ docs/ggplots_script.R
@@ -1,27 +1,28 @@
 ggplot(data = cars) +
   aes(x = speed) +
   aes(y = dist) +
-  geom_count(alpha = .5) +
+  geom_count(alpha = .5, size = 4) +
   ggxmean::geom_lm() +
   ggxmean::geom_lm_formula()
 ggplot(data = cars) +
   aes(x = speed) +
   aes(y = dist) +
-  geom_count(alpha = 2) +
+  geom_count(alpha = 2, size = 4) +
   ggxmean::geom_lm() +
   ggxmean::geom_lm_formula()
 ggplot(data = cars) +
   aes(x = speed) +
   aes(y = dist) +
-  geom_count(alpha = 2,
+  geom_count(alpha = 2, size = 4,
              color = "green") +
   ggxmean::geom_lm() +
   ggxmean::geom_lm_formula()
 ggplot(data = cars) +
   aes(x = speed) +
   aes(y = dist) +
-  geom_count(alpha = 2,
+  geom_count(alpha = 2, size = 4,
              color = "green",
              shape = "square") +
   ggxmean::geom_lm() +
   ggxmean::geom_lm_formula()
+ggplot() +
